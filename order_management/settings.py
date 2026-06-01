44c3aeae17b5fb551f4f506dfd3ad3b17a3a4540
Sheracore <m.ghaffari662@gmail.com>
2026-06-01 22:51:18 +0330

    faet(WIP): adding docker=

--- order_management/settings.py
+++ order_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a7x71p2-q1zx%!9_6s2wuc=t6qi%j3kob34zi-61at_&()ls*s'
+SECRET_KEY = os.getenv('SECRET_KEY'),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -90,8 +91,12 @@ WSGI_APPLICATION = 'order_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'order_db'),
+        'USER': os.getenv('POSTGRES_USER', 'order_user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'order_pass'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
